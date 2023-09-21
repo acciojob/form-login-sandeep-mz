d1363dc2714605ff2769fab58c0b701df739e1c0
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-21 13:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	
+	// Concatenate the first and last name
+	let fullName = `${form1.fname} ${form1.lname}`;
+	
+	// Use alert() to display the full name
+	alert(fullName);
 
 }
