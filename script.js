3e58152e8f272889a4d10ab4db748b81ca2892ea
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-21 13:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-	
-	// Concatenate the first and last name
-	let fullName = `${form1.fname} ${form1.lname}`;
-	
-	// Use alert() to display the full name
-	alert(fullName);
+    // Get the form element
+    let form = document.getElementById("form1");
 
+    // Get the values of the first name and last name fields
+    let firstName = form.elements.fname.value;
+    let lastName = form.elements.lname.value;
+
+    // Concatenate and display the first and last name in an alert
+    alert(firstName + " " + lastName);
 }
